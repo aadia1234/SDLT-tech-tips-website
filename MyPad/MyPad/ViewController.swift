d275bdba28940b24f1211685f70c50ce19aa3286
gam3boy1 <aadianand1234@gmail.com>
2021-03-07 20:53:53 +0800

    Initialize MyPad Project

---
+++ MyPad/MyPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPad
+//
+//  Created by Aadi Anand on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
