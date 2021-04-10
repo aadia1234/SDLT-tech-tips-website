e62f9dd3fc1b8fe0b8152f790558769c3c944356
Oranges <derekzhanghk@gmail.com>
2021-04-10 09:38:16 +0800

    Add files via upload

---
+++ TechHelpEXT/popup.js
@@ -0,0 +1,10 @@
+function QuerySaver() {
+    var x = document.getElementById("form");
+    // var text = "";
+    // var i;
+    // for (i = 0; i < x.length ;i++) {
+    //   text += x.elements[i].value + "<br>";
+    // }
+    // document.getElementById("demo").innerHTML = text;
+    alert(x);
+}
